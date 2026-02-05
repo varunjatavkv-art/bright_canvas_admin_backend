667ba2f7ac850c456bd5607d58bd7296d39f7ee8
Varun Jatav <varunjatav.kv@gmail.com>
2026-02-05 22:41:12 +0530

    asdas

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,15 @@
-// index.js: The entry point for Firebase Functions
-
-// Import V2 HTTP request handler
 import {onRequest} from "firebase-functions/v2/https";
-
-// Import V2 global configuration tools
 import {setGlobalOptions} from "firebase-functions/v2";
-// Note: We are keeping the logger import, but if your linter flags it as unused, 
-// you can safely remove the next line if you don't use it globally here.
-import {logger} from "firebase-functions/logger"; 
+import app from "../server.js"; 
 
-// Set global options for performance and cost control
 setGlobalOptions({maxInstances: 10});
 
-// --- IMPORTANT: Load your Express application ---
-// This requires the Express app to be exported using 'module.exports = app;' in server.js
-import app from "../server.js";
-
-// --- Export the Express App as a Single HTTP Function ---
-// The function name will be 'api' (e.g., your-project-id.cloudfunctions.net/api)
 export const api = onRequest(
-    // OPTIMIZATION: Set memory lower if 256MB is too much.
-    // The default is 256 MiB. Try 128 MiB for a very light API.
-    {
-      timeoutSeconds: 60, // Reduced from 300
-      maxInstances: 5,
-      region: "us-central1",
-      memory: "128MiB" // Explicitly set lower memory
-    }, 
-    app
+{
+ timeoutSeconds: 60,
+ maxInstances: 5,
+ region: "us-central1",
+ memory: "128MiB"
+}, 
+app
 );
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { db } from "./db.js";
 import cors from "cors";
 
-import { initializeApp } from "firebase-admin/app";
+// import { initializeApp } from "firebase-admin/app";
 import InvoiceRouter from "./routes/invoiceRouter.js";
 import blogRouter from "./routes/blogRouter.js";
 import workRouter from "./routes/workRouter.js";
@@ -10,17 +10,17 @@ import { WORK_DIR } from "./multer/work_multer.js";
 import { UPLOAD_DIR } from "./multer/blog_multer.js";
 import { INVOICE_DIR } from "./controller/invoiceController.js";
 
-
-const adminApp = initializeApp(); 
+const PORT = 8000;
+// initializeApp(); 
 
 const app = express();
 
 app.use(
-  cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "https://bright-canvas.web.app"],
-  methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"], 
-  allowedHeaders: ["Content-Type", "Authorization"],
-  })
+ cors({
+ origin: ["http://localhost:5173", "http://localhost:5174", "https://bright-canvas.web.app"],
+ methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"], 
+ allowedHeaders: ["Content-Type", "Authorization"],
+ })
 );
 
 app.use(express.json());
@@ -39,4 +39,19 @@ app.use("/api/blogs", blogRouter);
 app.use("/api/work", workRouter);
 app.use("/api/invoice", InvoiceRouter);
 
-export default app;
+app.use("/api", (req,res) => {
+ res.send("<h1>Hello World</h1>")
+})
+
+db.connectWithRetry();
+app.listen(PORT, ()=> {
+    console.log("Server is running on", PORT);
+    
+});
+// REMOVED app.listen() and db.connectWithRetry()
+
+// NOTE: You must ensure your DB connection logic is now triggered 
+// within the function execution context (e.g., inside your routers/controllers) 
+// or once globally if the function allows it.
+
+// export default app;
